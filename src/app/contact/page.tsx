76bc8cbb26089d0cc2210f95de12bb7784c043cd
Ilyes.dragovic <mingo.huncho@gmail.com>
2025-04-12 18:50:41 +0100

    lol

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-
-const Contact = () => {
-  return <div>Contact</div>;
+const Dashboard = () => {
+  return <div>Dashboard</div>;
 };
 
-export default Contact;
+export default Dashboard;
